ba971b13816776b161833a693f25ffa4d2816f1b
amaruk-fork <detectivezealot@163.com>
2017-02-26 22:33:18 +0800

    Add a C++ entry.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : MyIncubator.cpp
+// Author      : Amaruk
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "I'm back, C++." << endl; // prints I'm back, C++.
+	return 0;
+}
+
